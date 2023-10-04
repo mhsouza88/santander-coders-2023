6d0d755caa618e8de8b16bdd09a197c27623d03d
Maria Helena Souza <88038506+mhsouza88@users.noreply.github.com>
2023-10-04 19:10:38 -0300

    :sparkles: feat: Adicionando o main.js

---
+++ modulo-04/projeto-final/css/main.js
@@ -0,0 +1,21 @@
+body {
+    font-family: Arial, sans-serif;
+}
+
+.modo-escuro {
+    background-color: #333;
+    color: white;
+}
+
+.modo-escuro span {
+    color: white;
+}
+
+.modo-escuro .card {
+    background-color: #555;
+}
+
+.app {
+    width: 100%;
+    height: 100%;
+}
